1c1bad2187790e66ec02bd842052a06329205bc2
GeorgeClark <GeorgeClark@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-12-11 05:16:48 +0000

    Item9322: Rewrite to avoid Store API.
    git-svn-id: http://svn.foswiki.org/trunk/UpdateAttachmentsPlugin@10261 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/UpdateAttachmentsPlugin.pm
+++ lib/Foswiki/Plugins/UpdateAttachmentsPlugin.pm
@@ -15,23 +15,23 @@
 
 package Foswiki::Plugins::UpdateAttachmentsPlugin;
 use strict;
-use vars qw( $VERSION $RELEASE $SHORTDESCRIPTION $debug $pluginName $NO_PREFS_IN_TOPIC );
-$VERSION = '$Rev$';
-$RELEASE = 'Foswiki-1.0';
-$SHORTDESCRIPTION = 'A batched alternative to AutoAttachments (adds and removes attachements)';
-$NO_PREFS_IN_TOPIC = 1;
-$pluginName = 'UpdateAttachmentsPlugin';
+
+our $VERSION = '$Rev$';
+our $RELEASE = 'Foswiki-1.0';
+our $SHORTDESCRIPTION = 'A batched alternative to AutoAttachments (adds and removes attachements)';
+our $NO_PREFS_IN_TOPIC = 1;
+
+my $debug = 0;
 
 sub initPlugin {
     my( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if( $Foswiki::Plugins::VERSION < 1.026 ) {
-        Foswiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
+    if( $Foswiki::Plugins::VERSION < 2.1 ) {
+        Foswiki::Func::writeWarning( "Version mismatch between UpdateAttachmentsPlugin  and Plugins.pm" );
         return 0;
     }
     
-    my $setting = $Foswiki::cfg{Plugins}{UpdateAttachmentsPlugin}{ExampleSetting} || 0;
     $debug = $Foswiki::cfg{Plugins}{UpdateAttachmentsPlugin}{Debug} || 0;
     Foswiki::Func::registerRESTHandler('update', \&restUpdate);
     return 1;
@@ -46,6 +46,7 @@ sub restUpdate {
     print STDERR "update the attachments of $web\n" if $debug;
     
     #force autoattach off, as we need to know the real META
+    my $cfgAutoAttach = $Foswiki::cfg{AutoAttachPubFiles};
     $Foswiki::cfg{AutoAttachPubFiles} = 0;
     
     #TODO: consider the lighter weight aproach of scanning timestamps in the pub dir
@@ -54,101 +55,207 @@ sub restUpdate {
     
     my $topicsTested = 0;
     my $topicsUpdated = 0;
-    my $attachmentsRemoved = 0;
     my $attachmentsIgnored = 0;
+    my $attachmentsRemoved = 0;
+    my $attachmentsAdded = 0;
+    my $attachmentsUpdated = 0;
+
     #TODO: test user's access to web (rest already tests for web CHANGE access)
     my @topicNames = Foswiki::Func::getTopicList( $web );
     foreach my $topic (@topicNames) {
         # test user's permission on this topic (do this first, eventually this check may not read the topic.. for now it will populate the cache)
         next if (!Foswiki::Func::checkAccessPermission( 'CHANGE', Foswiki::Func::getWikiName(), undef, $topic, $web ));
 
-        my( $meta, $text ) = Foswiki::Func::readTopic( $web, $topic );
-        my @knownAttachments = $meta->find('FILEATTACHMENT');
-        my @attachmentsFoundInPub = Foswiki::Store::_findAttachments($store, $web, $topic, \@knownAttachments);
-        
         my $changed = 0;
-        
-        #new or updated file
-        foreach my $foundAttachment (@attachmentsFoundInPub) {
-            my ( $fileName, $origName ) =
-                Foswiki::Sandbox::sanitizeAttachmentName( $foundAttachment->{name} );
-            #ignore filenames that would need to be renamed
-            if ($fileName ne $origName) {
-                print STDERR "ERROR: UpdateAttachments: ignoring $origName, in $web.$topic - not a valid Foswiki Attachment filename";
-                $attachmentsIgnored++;
-                next;
+
+        my $topicObject = Foswiki::Meta->load($session, $web, $topic);
+        my @knownAttachments = $topicObject->find('FILEATTACHMENT');
+        my ($attachmentsFoundInPub, $attachmentsRemovedFromMeta, $attachmentsAddedToMeta, $attachmentsUpdatedInMeta) =
+          synchroniseAttachmentsList($topicObject, \@knownAttachments );
+        my @validAttachmentsFound;
+        foreach my $foundAttachment (@$attachmentsFoundInPub) {
+
+            # test if the attachment filename is valid without having to
+            # be sanitized. If not, ignore it.
+            my $validated = Foswiki::Sandbox::validateAttachmentName(
+                $foundAttachment->{name} );
+            unless ( defined $validated
+                && $validated eq $foundAttachment->{name} )
+            {
+
+                print STDERR 'AutoAttachPubFiles ignoring '
+                  . $foundAttachment->{name} . ' in '
+                  . $topicObject->getPath()
+                  . ' - not a valid Foswiki Attachment filename';
             }
-
-            #default attachment owner to {AttachAsUser}
-            if ((defined($Foswiki::cfg{Plugins}{UpdateAttachmentsPlugin}{AttachAsUser})) &&
-                ($Foswiki::cfg{Plugins}{UpdateAttachmentsPlugin}{AttachAsUser} ne '')) {
-                $foundAttachment->{user} = $Foswiki::cfg{Plugins}{UpdateAttachmentsPlugin}{AttachAsUser};
-                #4.2.0 falls back and works if WikiName there.
-                #TODO: needs testing for the generalised usermapping case - shoudl store cUID
-            }
-
-            my $existingFile = $meta->get( 'FILEATTACHMENT', $foundAttachment->{name} );
-            if ( !$existingFile ) {
-                #if its not in @knownAttachments, add it
-                $meta->putKeyed('FILEATTACHMENT', $foundAttachment );
-                $changed = 1;
-            } else {
-                #if its @knownAttachments, update _if_ attr's have changed
-                if (
-                    (!defined($existingFile->{user})) || ($foundAttachment->{user} ne $existingFile->{user}) ||
-                    (!defined($existingFile->{date})) || ($foundAttachment->{date} ne $existingFile->{date}) ||
-                    (!defined($existingFile->{attr})) || ($foundAttachment->{attr} ne $existingFile->{attr}) ||
-                    (!defined($existingFile->{size})) || ($foundAttachment->{size} != $existingFile->{size})
-                   ){
-                    $foundAttachment->{comment} = $existingFile->{comment};
-                    $foundAttachment->{attr} = $existingFile->{attr};
-                    #if attached by a real user, keep them.
-                    if ((defined($existingFile->{user})) && (      # the default from 4.2.0 onwards
-                        ($existingFile->{user} ne '') &&            
-                        ($existingFile->{user} ne 'UnknownUser')    # the 4.1.2 autoattach default
-                        )) {
-                        $foundAttachment->{user} = $existingFile->{user};
-                    }
-
-                    $meta->putKeyed('FILEATTACHMENT', $foundAttachment );
-                    $changed = 1;
-                }
+            else {
+                push @validAttachmentsFound, $foundAttachment;
             }
         }
-        #RemoveMissing
-        if ( defined($Foswiki::cfg{Plugins}{UpdateAttachmentsPlugin}{RemoveMissing}) &&
-           ($Foswiki::cfg{Plugins}{UpdateAttachmentsPlugin}{RemoveMissing} == 1)) {
-            foreach my $knownAttachment (@knownAttachments) {
-                #if not in @attachmentsFoundInPub, remove from @knownAttachments
-                if (!grep(/$knownAttachment->{name}/,
-                        map( {$_->{name}} @attachmentsFoundInPub)
-                          ) ) {
-                    $meta->remove('FILEATTACHMENT', $knownAttachment->{name} );
-                    $changed = 1;
-                    $attachmentsRemoved++;
-                }
-            }
-        }
+
+        $topicObject->putAll( 'FILEATTACHMENT', @validAttachmentsFound )
+          if @validAttachmentsFound;
+
 
         #TODO: actually test that they are the same! (update size, date etc)
-        #print STDERR "$web.$topic has ".scalar(@knownAttachments)." in meta, ".scalar(@attachmentsFoundInPub)." in pub dir\n" if $debug;
+        print STDERR "$web.$topic has ".scalar(@knownAttachments)." in meta, ".scalar(@$attachmentsFoundInPub)." in pub dir\n" if $debug;
+
+        $changed = scalar(@$attachmentsRemovedFromMeta) + scalar(@$attachmentsAddedToMeta) + scalar(@$attachmentsUpdatedInMeta);
+
         if ($changed) {
-            if ( defined($Foswiki::cfg{Plugins}{UpdateAttachmentsPlugin}{UseDangerousNoHandlersSave}) &&
-                ($Foswiki::cfg{Plugins}{UpdateAttachmentsPlugin}{UseDangerousNoHandlersSave} == 1)) {
-                #can I leave the user = undef?
-                $session->{store}->_noHandlersSave( $Foswiki::Plugins::SESSION->{user}, $web, $topic, $text, $meta, { comment => 'UpdateAttachments' } );
-            } else {
-                Foswiki::Func::saveTopic( $web, $topic, $meta, $text, { comment => 'UpdateAttachments' } );
-            }
+            $topicObject->save(  comment => 'UpdateAttachments' );
             print STDERR "updating the attachments of $web.$topic\n" if $debug;
             $topicsUpdated++;
+            $attachmentsRemoved += scalar(@$attachmentsRemovedFromMeta);
+            $attachmentsAdded += scalar(@$attachmentsAddedToMeta);
+            $attachmentsUpdated += scalar(@$attachmentsUpdatedInMeta);
         }
         $topicsTested++;
         #TODO: $Foswiki::cfg{Plugins}{UpdateAttachmentsPlugin}{BatchLimit}
+
     }
 
-    print STDERR "UpdateAttachments checked $topicsTested, updated $topicsUpdated, removed $attachmentsRemoved attachments, $attachmentsIgnored ignored" if $debug;
-    return "UpdateAttachments checked $topicsTested, updated $topicsUpdated, removed $attachmentsRemoved attachments, $attachmentsIgnored ignored";
+    print STDERR "UpdateAttachments checked $topicsTested, updated $topicsUpdated,   removed $attachmentsRemoved attachments, $attachmentsIgnored ignored" if $debug;
+
+    # Restore auto-attach setting.   (This *really* ought to be disabled if using this plugin
+    $Foswiki::cfg{AutoAttachPubFiles} = $cfgAutoAttach ;
+
+    return "UpdateAttachments Topics checked $topicsTested, updated $topicsUpdated, <br/> Attachments updated $attachmentsUpdated, added $attachmentsAdded, removed $attachmentsRemoved attachments, $attachmentsIgnored ignored";
+}
+
+=begin TML
+
+---++ ObjectMethod synchroniseAttachmentsList(\@old) -> @new
+
+*Copied directly from Foswiki::Store::VC::Handler.pm*
+
+Synchronise the attachment list from meta-data with what's actually
+stored in the DB. Returns an ARRAY of FILEATTACHMENTs. These can be
+put in the new tom.
+
+=cut
+
+# IDEA On Windows machines where the underlying filesystem can store arbitary
+# meta data against files, this might replace/fulfil the COMMENT purpose
+
+sub synchroniseAttachmentsList {
+    my ( $topicObject, $attachmentsKnownInMeta ) = @_;
+
+    my %filesListedInPub  = _getAttachmentStats($topicObject);
+    my %filesListedInMeta = ();
+    my %filesToAddToMeta = ();
+    my @filesRemovedFromMeta = ();
+    my @filesAddedToMeta = ();
+    my @filesUpdatedInMeta = ();
+
+    # You need the following lines if you want metadata to supplement
+    # the filesystem
+    if ( defined $attachmentsKnownInMeta ) {
+        %filesListedInMeta =
+          map { $_->{name} => $_ } @$attachmentsKnownInMeta;
+    }
+
+    foreach my $file ( keys %filesListedInPub ) {
+        if ( $filesListedInMeta{$file} ) {
+            if ( $filesListedInMeta{$file}{size} ne $filesListedInPub{$file}{size} ||
+                 $filesListedInMeta{$file}{date} ne $filesListedInPub{$file}{date} ) {
+                $filesListedInPub{$file}{autoattached} = "1";
+                push @filesUpdatedInMeta, $file;
+                print STDERR "Updating $file \n";
+            }
+            # Bring forward any missing yet wanted attribute
+            foreach my $field qw(comment attr user version autoattached) {
+                if ( $filesListedInMeta{$file}{$field} ) {
+                    $filesListedInPub{$file}{$field} =
+                      $filesListedInMeta{$file}{$field};
+                }
+            }
+        }
+        else {
+            #default attachment owner to {AttachAsUser}
+            push @filesAddedToMeta, $file;
+            print STDERR "Adding $file \n";
+            $filesListedInPub{$file}{autoattached} = "1";
+            if ((defined($Foswiki::cfg{Plugins}{UpdateAttachmentsPlugin}{AttachAsUser})) &&
+                ($Foswiki::cfg{Plugins}{UpdateAttachmentsPlugin}{AttachAsUser} ne '')) {
+                $filesListedInPub{$file}{user} = $Foswiki::cfg{Plugins}{UpdateAttachmentsPlugin}{AttachAsUser};
+                #TODO: needs testing for the generalised usermapping case - shoudl store cUID
+            }
+        }
+    }
+
+    # A comparison of the keys of the $filesListedInMeta and %filesListedInPub
+    # would show files that were in Meta but have disappeared from Pub.
+
+    foreach my $file ( keys %filesListedInMeta ) {
+        if (! $filesListedInPub{$file} ) {
+            print STDERR "Removing $file \n";
+            push @filesRemovedFromMeta, $file;
+        }
+    }
+
+    # Do not change this from array to hash, you would lose the
+    # proper attachment sequence
+    my @deindexedBecauseMetaDoesnotIndexAttachments = values(%filesListedInPub);
+
+    return \@deindexedBecauseMetaDoesnotIndexAttachments, \@filesRemovedFromMeta, \@filesAddedToMeta, \@filesUpdatedInMeta;
+}
+
+=begin TML
+
+---++ ObjectMethod getAttachmentList() -> @list
+
+Get list of attachment names actually stored for topic.
+
+=cut
+
+sub getAttachmentList {
+    my $topicObject = shift;
+    my $dir  = "$Foswiki::cfg{PubDir}/" . $topicObject->web() . "/" . $topicObject->topic();
+    my $dh;
+    opendir( $dh, $dir ) || return ();
+    my @files = grep { !/^[.*_]/ && !/,v$/ } readdir($dh);
+    closedir($dh);
+    return @files;
+}
+
+# returns {} of filename => { key => value, key2 => value }
+# for any given web, topic
+sub _getAttachmentStats {
+    my $topicObject           = shift;
+    my %attachmentList = ();
+    my $dir            = "$Foswiki::cfg{PubDir}/" . $topicObject->web() . '/' . $topicObject->topic();
+    foreach my $attachment ( getAttachmentList($topicObject) ) {
+        my @stat = stat( $dir . "/" . $attachment );
+        $attachmentList{$attachment} =
+          _constructAttributesForAutoAttached( $attachment, \@stat );
+    }
+    return %attachmentList;
+}
+
+# as long as stat is defined, return an emulated set of attributes for that
+# attachment.
+sub _constructAttributesForAutoAttached {
+    my ( $file, $stat ) = @_;
+
+    my %pairs = (
+        name    => $file,
+        version => '',
+        path    => $file,
+        size    => $stat->[7],
+        date    => $stat->[9],
+
+#        user    => 'UnknownUser',  #safer _not_ to default - Foswiki will fill it in when it needs to
+        comment      => '',
+        attr         => '',
+    );
+
+    if ( $#$stat > 0 ) {
+        return \%pairs;
+    }
+    else {
+        return;
+    }
 }
 
 1;
--- lib/Foswiki/Plugins/UpdateAttachmentsPlugin/Config.spec
+++ lib/Foswiki/Plugins/UpdateAttachmentsPlugin/Config.spec
@@ -1,4 +1,5 @@
-#---+ Update Attachments Plugin
+#---++ Extensions
+#---++ Update Attachments Plugin
 # **STRING**
 # To attribute attachments to a known user, set this to their WikiName. This user should exist,
 # and be mappable to a login. 
@@ -7,6 +8,7 @@ $Foswiki::cfg{Plugins}{UpdateAttachmentsPlugin}{AttachAsUser} = '';
 # remove references to attachments that no longer exist in pub
 $Foswiki::cfg{Plugins}{UpdateAttachmentsPlugin}{RemoveMissing} = $FALSE;
 # **BOOLEAN**
-# use the _internal_ _noHandlersSave - this may break in future if the internal method is changed
-# not recomended unless you know the code.
+# use the _internal_ _noHandlersSave - This option causes the topic update code to write directly into the
+# Store, bypassing the API's and other Handlers.  This is *Strongly* not recommended.  This may break in 
+# future and is not recomended unless you know the code.
 $Foswiki::cfg{Plugins}{UpdateAttachmentsPlugin}{UseDangerousNoHandlersSave} = $FALSE;
