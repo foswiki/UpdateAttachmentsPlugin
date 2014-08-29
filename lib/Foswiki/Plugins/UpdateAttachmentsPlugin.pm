35934778360d9c27ff3d821e1deaad51ff20bbb4
MichaelDaum <github@michaeldaumconsulting.com>
2014-08-29 18:15:28 +0200

    Item13001: fully specify security setting for REST

--- lib/Foswiki/Plugins/UpdateAttachmentsPlugin.pm
+++ lib/Foswiki/Plugins/UpdateAttachmentsPlugin.pm
@@ -5,8 +5,8 @@ use warnings;
 use Foswiki::Plugins ();
 use Foswiki::Func ();
 
-our $VERSION = '3.01';
-our $RELEASE = '3.01';
+our $VERSION = '3.10';
+our $RELEASE = '3.10';
 our $SHORTDESCRIPTION = 'A batched alternative to AutoAttachments (adds and removes attachements)';
 our $NO_PREFS_IN_TOPIC = 1;
 our $core;
@@ -30,7 +30,11 @@ sub initPlugin {
     return 0;
   }
 
-  Foswiki::Func::registerRESTHandler('update', sub { core->restUpdate(@_) }, authenticate => 1);
+  Foswiki::Func::registerRESTHandler('update', sub { core->restUpdate(@_) }, 
+    authenticate => 1,
+    validate => 0,
+    http_allow => 'GET,POST',
+  );
 
   #force autoattach off
   $Foswiki::cfg{AutoAttachPubFiles} = 0;
